f60932cc0c8a7cbba2c8b355bc1a5312423e0084
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-03-24 14:16:17 -0400

    Create initialize and readers for children, parent, value

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
-  
+    # attr_reader :parent, :children, :value
+
+    def initialize(node_name)
+        @new_node = node_name
+        @parent = nil
+        @children = []
+    end
+
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def value
+        @new_node
+    end
+
+    # def parent=(property)
+    #     @children << 
+    # end
 end
